c57b53e3627d39c0d62320db7de704799befd290
lam <ldl020590@yahoo.fr>
2021-01-16 13:42:04 +0100

    Ajout balise meta scale

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -41,7 +41,7 @@ async function loadMap(position) {
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        maxZoom: 30,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
